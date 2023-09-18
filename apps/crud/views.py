c1998b81644284ed6110c6737acf9becf791a145
DESKTOP-DKER1IU\user <f125678@naver.com>
2023-09-18 09:32:05 +0900

    환경설정

---
+++ apps/crud/views.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, redirect, render_template, url_for
+from apps.app import db
+from apps.crud.models import User
+
+crud = Blueprint(
+    "crud",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+)
+
+@crud.route("/")
+def index():
+    return render_template("crud/index.html")
+
+@crud.route("/sql")
+def sql():
+    db.session.query(User).all()
+    return "콘솔 로그를 확인해 주세요"
